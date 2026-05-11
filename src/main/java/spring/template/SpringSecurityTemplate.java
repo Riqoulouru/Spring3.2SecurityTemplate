c47cd70cd630c7b7cc4c16eb22642d0262913d01
Basile <riqoulouru@gmail.com>
2026-05-11 11:18:34 +0200

    refacto : change names of project

---
+++ src/main/java/spring/template/SpringSecurityTemplate.java
@@ -0,0 +1,13 @@
+package spring.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTemplate {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTemplate.class, args);
+	}
+
+}
